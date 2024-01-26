abb9832c61357535b4ed349aaadc4b094e66be5f
Lewis <lewis1broadhurst@gmail.com>
2024-01-26 21:15:57 +0000

    start on logger

---
+++ LoggingCS/AbstractLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradingEngineServer.Logging;
+
+namespace TradingEngine.Logging
+{
+    public abstract class AbstractLogger : ILogger
+    {
+        protected AbstractLogger() { }
+
+        public void Debug(string module, string message) => Log(LogLevel.Debug, module, message);
+        public void Debug(string module, Exception exception) => Log(LogLevel.Debug, module, $"{exception}");
+
+        public void Information(string module, string message) => Log(LogLevel.Info, module, message);
+        public void Information(string module, Exception exception) => Log(LogLevel.Info, module, $"{exception}");
+
+        public void Warning(string module, string message) => Log(LogLevel.Warn, module, message);
+        public void Warning(string module, Exception exception) => Log(LogLevel.Warn, module, $"{exception}");
+
+        public void Error(string module, string message) => Log(LogLevel.Error, module, message);
+        public void Error(string module, Exception exception) => Log(LogLevel.Error, module, $"{exception}");
+
+        protected abstract void Log(LogLevel level, string module,string message);
+    }
+}
---
+++ LoggingCS/ILogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingEngine.Logging
+{
+    internal interface ILogger
+    {
+        // module is where the code was called from
+        void Debug(string module, string message);
+        void Debug(string module, Exception exception);
+
+        void Information(string module, string message);
+        void Information(string module, Exception exception);
+
+        void Warning(string module, string message);
+        void Warning(string module, Exception exception);
+
+        void Error(string module, string message);
+        void Error(string module, Exception exception);
+    }
+}
