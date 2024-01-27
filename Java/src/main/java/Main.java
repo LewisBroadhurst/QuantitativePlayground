3b4284b0e8c82645fda2927959a65860aec2a800
Lewis <lewis1broadhurst@gmail.com>
2024-01-27 13:30:59 +0000

    adding java

---
+++ Java/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    // So any main class is runnable and instantiates a Java runner...
+    // From this runner we can link out to other resources and processes in our application
+    // e.g. For a Spring Project, we create a bunch of endpoints that we can request resources from
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        int result = 4 * 5;
+        System.out.println(result);
+
+//        System.out.println("Goodbye World");
+    }
+}
