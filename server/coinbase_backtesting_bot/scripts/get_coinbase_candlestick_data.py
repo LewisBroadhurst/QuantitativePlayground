b44d2da5bc2b699928ee7dfe6ac238e1330134af
Lewis <lewis1broadhurst@gmail.com>
2024-02-04 22:25:56 +0000

    Finished coinbase data gathering script

--- server/coinbase_backtesting_bot/scripts/get_coinbase_candlestick_data.py
+++ server/coinbase_backtesting_bot/scripts/get_coinbase_candlestick_data.py
@@ -1,6 +1,10 @@
 import os
 import datetime as dt
 import sys
+import time
+import pickle
+import pandas as pd
+
 sys.path.append("../../")
 
 from coinbase.rest import RESTClient
@@ -16,19 +20,16 @@ secrets = dotenv_values(".env")
 api_key = os.getenv('COINBASE_API_KEY_TRADING_040224')
 api_secret = os.getenv('COINBASE_API_SECRET_TRADING_040224')
 client = RESTClient(api_key=api_key, api_secret=api_secret)
-filename_prefix = "COINBASE_CANDLESTICKS_"
+filename_prefix = "COINBASE_CANDLESTICKS"
+sleep = 5
 
 ## Script to get Cryptocurrency Candles from Coinbase.
-## Max 300 at once.
 if __name__ == "__main__":
-    ## Converter for UNIX into standard time useful for better file names
-    ## Convert response into .pkl file
-    ## If number of candles > 300 split into multiple requests, then join data together.
-    
+
     # 1. Enter time dates the script should retrive from and to
     # e.g. dt.datetime(2023, 1, 1, 14, 30).timestamp()
-    datetime_from = dt.datetime(2023, 1, 1)
-    datetime_to = dt.datetime(2024, 1, 1)
+    datetime_from = dt.datetime(2022, 1, 1)
+    datetime_to = dt.datetime(2022, 1, 2)
     
     # 2. Enter Granularity [ONE_MINUTE... FIFTEEN_MINUTE... ONE_DAY...] and Product [BTC-USD, ETH-USD...]
     granularity = "ONE_DAY"
@@ -46,20 +47,51 @@ if __name__ == "__main__":
     print(f"Getting the candlestick data for {product} from {datetime_from} to {datetime_to}.")
     print(f"This will require {number_of_requests_required} requests to get {number_of_candlesticks} candlesticks.")
     
+    candlestick_data = []
+    
     if number_of_requests_required == 1:
-        candles_response = client.get_candles(product, datetime_from.timestamp(), datetime_to.timestamp(), granularity)
-        candles_list = candles_response['candles']
+        candles_response = client.get_candles(product, int(datetime_from.timestamp()), int(datetime_to.timestamp()), granularity)
+        candlestick_data = candles_response['candles']
         
-        # Check if .pkl file exists with same name
-        # If yes look to add _1, _2, _3 in a while loop until its a new file name
     else:
         length_of_time_for_segment = time_range / number_of_requests_required
+        current_segment_start = datetime_from
         current_segment_end = datetime_from + length_of_time_for_segment
-        candlestick_data_segments = []
         
         for i in range(1, number_of_requests_required):
-            candles_response = client.get_candles(product, datetime_from.timestamp(), datetime_to.timestamp(), granularity)
+            candles_response = client.get_candles(product, int(current_segment_start.timestamp()), int(current_segment_end.timestamp()), granularity)
             candles_list = candles_response['candles']
+            
+            current_segment_start += length_of_time_for_segment
+            current_segment_end += length_of_time_for_segment
+            
+            candlestick_data.extend(candles_list)
+            
+            if i < number_of_requests_required:
+                print(f"Sleeping for {sleep} seconds to not spam coinbase API on bigger calls ({i} / {number_of_requests_required}).")
+                time.sleep(sleep)
+                        
+    filename_suffix = f"{int(datetime_from.timestamp() / 1_000_000)}_{int(datetime_to.timestamp() / 1_000_000)}.pkl"
+    filename = f"{filename_prefix}_{product}_{granularity}_{filename_suffix}"
+    
+    print(f"Data will be saved under the filename {filename}.")
+    
+    with open(filename, 'wb') as file:
+        
+        df = pd.DataFrame(candlestick_data)
+        df['start'] = df["start"].astype(int)
+        df["candle_datetime"] = pd.to_datetime(df["start"], unit='s')
+        
+        df_renamed = df.rename(columns={'start': 'candle_open_posix'})
+        df_renamed = df_renamed[["candle_datetime", "candle_open_posix", "low", "high", "open", "close", "volume"]]
+        
+        sorted_df = df_renamed.sort_values(by="candle_open_posix", ascending=True)
+        sorted_df.reset_index(drop=True, inplace=True)
+        pickle.dump(sorted_df, file)
+        
+        print(sorted_df.head())
+        
+    print("Data successfully pickled. Yum.")
         
         
         
